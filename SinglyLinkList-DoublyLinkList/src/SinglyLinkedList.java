2eceb1a8bc46048bd8f636c7564631541abbcbdc
Dexshine <b_o_y_s@hotmail.com>
2021-08-03 02:49:08 +0700

    first build

--- SinglyLinkList-DoublyLinkList/src/SinglyLinkedList.java
+++ SinglyLinkList-DoublyLinkList/src/SinglyLinkedList.java
@@ -7,7 +7,7 @@ public class SinglyLinkedList {
         this.listName = name;
         this.head = null;
     }
-
+    //remove last Node of SinglyLinkedList.
     public void popBack(){
     if(isEmpty()){
         System.out.println("ERROR");
@@ -17,7 +17,7 @@ public class SinglyLinkedList {
         current.next = null;
     }
     }
-
+    //remove first Node of SinglyLinkedList.
     public void popFront(){
     if(isEmpty()){
         System.out.println("ERROR");
@@ -25,7 +25,7 @@ public class SinglyLinkedList {
         this.head = this.head.next;
     }
     }
-
+    //return first Node of SinglyLinkedList.
     public Node topFront(){
     if(isEmpty()){
         System.out.println("ERROR");
@@ -34,7 +34,7 @@ public class SinglyLinkedList {
         return this.head;
     }
     }
-
+    //return last Node of SinglyLinkedList.
     public Node topBack(){
     if(isEmpty()){
         System.out.println("ERROR");
@@ -45,7 +45,7 @@ public class SinglyLinkedList {
         return current;
     }
     }
-
+    //push new Node in front of first Node of SinglyLinkedList.
     public void pushFront(Node node){
     if(isEmpty()){
         this.head = node;
@@ -55,7 +55,7 @@ public class SinglyLinkedList {
         this.head = node;
     }
     }
-
+    //push new Node at the back of last Node of SinglyLinkedList.
     public void pushBack(Node node) {
     if (isEmpty()){
         this.head = node;
@@ -66,7 +66,7 @@ public class SinglyLinkedList {
         current.next = node;
     }
     }
-
+    //find Node that have same student_id as argument.
     public Node findNode(int id){
     if(isEmpty()){
         return new Node("Empty List!");
@@ -79,7 +79,7 @@ public class SinglyLinkedList {
         return new Node("Student Not Found!");
     }
     }
-    
+    //erase Node that have same student_id as argument.
     public Node eraseNode(int id){
     if(isEmpty()){
         System.out.println("ERROR");
@@ -97,19 +97,19 @@ public class SinglyLinkedList {
         return new Node("Student Not Found!");
     }
     }
-    
+    //add node2 behind node1.
     public void addNodeAfter(Node node1, Node node2){
         node2.next = node1.next;
         node1.next = node2;
     }
-    
+    //add node2 in front of node1.
     public void addNodeBefore(Node node1, Node node2){
         Node current = this.head;
         while(current.next != node1) current = current.next;
         node2.next = current.next;
         current.next = node2;
     }
-    
+    //check if SinglyLinkedList is empty or not.
     public boolean isEmpty(){
         return this.head == null;
     }
@@ -118,7 +118,7 @@ public class SinglyLinkedList {
         while(current.next != null) current = current.next;
         current.next = list.head;
     }
-    
+    //print student_id of all Node in SinglyLinkedList.
     public void printStructure(){
         if(isEmpty()){
             System.out.println(this.listName + ": head -> null");
@@ -132,7 +132,7 @@ public class SinglyLinkedList {
             System.out.println(" -> null");
         }
     }
-    
+    //find who got highestGPA in SinglyLinkedList.
     public Node whoGotHighestGPA(){
         if(isEmpty()){
             return new Node("Empty List!");
